df737c2c8154aa2bd204c1af87814aa15836549a
RacingTomcat <alexander.stier@cutiundstier.de>
2016-07-13 10:59:20 +0200

    VisualStudio2015 Config

---
+++ Hyper-V disable.cmd
@@ -0,0 +1,2 @@
+C:\Windows\System32\bcdedit.exe /set {current} hypervisorlaunchtype off
+shutdown /r /f /t 0
---
+++ Hyper-V enable.cmd
@@ -0,0 +1,2 @@
+C:\Windows\System32\bcdedit.exe /set {current} hypervisorlaunchtype auto
+shutdown /r /f /t 0
