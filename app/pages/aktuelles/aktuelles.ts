96c8f90382169bf33a2b83d3f34ce605b7bd7cdd
Alexander Stier <Alexander Stier>
2016-09-23 01:12:11 +0200

    - Link to Facebook-Post

--- app/pages/aktuelles/aktuelles.ts
+++ app/pages/aktuelles/aktuelles.ts
@@ -10,6 +10,7 @@ import { FacebookService } from '../../providers/facebook-service/facebook-servi
 export class AktuellesPage {
 
     title = 'Aktuelles';
+    page_id: string = 'PrinzengardeGL'; // YOUR_PAGE_ID_HERE
     public facebook_feed: any;
 
     constructor(private navController: NavController, public facebookService: FacebookService) {
@@ -25,11 +26,16 @@ export class AktuellesPage {
         });
     }
 
-    showmore(id) {
+    show_more(id) {
         var content_div = document.getElementById(id);
         content_div.className = "expand";
         var content_button = document.getElementById(id+'_button');
         content_button.className = "expand_hide";
+    }
+
+    open_facebook(id) {
+        var ids = id.split("_");
+        var win = window.open("https://www.facebook.com/"+this.page_id+"/posts/"+ids[1], "_blank");
     }
 
 }
